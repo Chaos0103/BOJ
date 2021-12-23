a30b4ef932812ac86ae14eb03695cc02392edcf5
chaos0103 <lyt1228@naver.com>
2021-12-23 19:51:04 +0900

    성공

---
+++ 그룹 단어 체커.py
@@ -0,0 +1,24 @@
+# 1316번
+# 실버5
+
+import sys
+
+n = int(input())
+
+cnt = 0
+for _ in range(n):
+    line = sys.stdin.readline().rstrip()
+    c = 1
+    for i in range(len(line)):
+        s = 1
+        for j in range(i, len(line)):
+            if line[i] != line[j]:
+                s = 0
+            if line[i] == line[j] and s == 0:
+                c = 0
+                break
+        if c == 0:
+            break
+    cnt += c
+
+print(cnt)
---
+++ 셀프 넘버.py
@@ -0,0 +1,19 @@
+# 4673번
+# 실버5
+
+def d(n):
+    A = n // 10000
+    B = n // 1000 % 10
+    C = n // 100 % 10
+    D = n // 10 % 10
+    E = n % 10
+    return int(n+A+B+C+D+E)
+
+
+data = [0] * 20000
+for i in range(10000):
+    data[d(i)] += 1
+
+for i in range(1, 10000):
+    if data[i] == 0:
+        print(i)
