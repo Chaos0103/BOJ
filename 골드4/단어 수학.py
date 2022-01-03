ea19639ed91396d25ed99d01eee78802669a6fb3
chaos0103 <lyt1228@naver.com>
2022-01-04 00:41:51 +0900

    1339번

--- 골드4/단어 수학.py
+++ 골드4/단어 수학.py
@@ -1,13 +1,30 @@
-n = int(input())
-data = [input() for _ in range(n)]
+import sys
 
-data.sort(reverse=True, key= lambda x:len(x))
-num = dict()
+n = int(sys.stdin.readline())
+data = [sys.stdin.readline().rstrip() for _ in range(n)]
 
-maxLen = len(data[0])
-cnt = 9
-idx = 0
-for i in range(n):
-    if data[i][idx] not in num:
-        num[data[i][idx]] = cnt
-        cnt -= 1
+data.sort(reverse=True, key=lambda x:len(x))
+cnt = dict()
+
+l = len(data[0])
+for i in range(l):
+    for j in cnt:
+        cnt[j] *= 10
+    for line in data:
+        if l-i > len(line):
+            continue
+        idx = i - (l - len(line))
+        if line[idx] in cnt:
+            cnt[line[idx]] += 1
+        else:
+            cnt[line[idx]] = 1
+
+cntSort = sorted(cnt.items(), key=lambda item:item[1], reverse=True)
+num = 9
+result = 0
+
+for idx in range(len(cntSort)):
+    result += num * cntSort[idx][1]
+    num -= 1
+
+print(result)
