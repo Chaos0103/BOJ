a3aee3952d944e56fe3be12ea121f9911b85b9c3
chaos0103 <lyt1228@naver.com>
2022-02-04 14:28:52 +0900

    ATM

--- Silver3/11399.py
+++ Silver3/11399.py
@@ -2,10 +2,9 @@ n = int(input())
 data = list(map(int, input().split()))
 
 data.sort()
-
 result = 0
+
 for i in range(n):
-    for j in range(i+1):
-        result += data[j]
+    result += sum(data[:i+1])
 
 print(result)
