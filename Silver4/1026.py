fb1e1fa0972a594e6074b5882557bb0a9e07b220
chaos0103 <lyt1228@naver.com>
2022-02-04 15:26:33 +0900

    보물

--- Silver4/1026.py
+++ Silver4/1026.py
@@ -1,12 +1,24 @@
 n = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+array_A = list(map(int, input().split()))
+array_B = list(map(int, input().split()))
 
-A.sort(reverse=True)
-B.sort()
+data = [-1] * n
+array_A.sort()
+
+for i in range(n):
+    max_value = -1
+    idx = 0
+    for j in range(n):
+        if array_B[j] > max_value and data[j] == -1:
+            idx = j
+            max_value = array_B[j]
+    data[idx] = array_A[i]
+
+for i in range(n):
+    array_A[i] = data[i]
 
 result = 0
 for i in range(n):
-    result += A[i]*B[i]
+    result += array_A[i] * array_B[i]
 
 print(result)
