8600da2c6a7e7379ebb8df3f876e8c5d7c15c8f7
chaos0103 <lyt1228@naver.com>
2022-02-04 16:14:32 +0900

    잃어버린 괄호

--- Silver2/1541.py
+++ Silver2/1541.py
@@ -2,19 +2,15 @@ line = input().split('-')
 
 data = []
 
-for l in line:
+for d in line:
     num = 0
-    sdata = l.split('+')
-    for n in sdata:
+    ldata = d.split('+')
+    for n in ldata:
         num += int(n)
     data.append(num)
 
-result = data[0]
-for i in range(1, len(data)):
-    result -= data[i]
+result = data[0] * 2
+for n in data:
+    result -= n
 
 print(result)
-
-# 알고리즘은 맞았는데 구현에서 틀림
-# split 활용
-# eval은 0으로 시작하면 적용 불가
